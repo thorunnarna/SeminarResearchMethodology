7321a3d7881b90b1e8c33336ee31a970d2851c05
thorunnarna <thorunnarna93@gmail.com>
2019-03-19 21:23:33 +0100

    Update for the Twitter question

--- CourseworkA/your_twitter.R
+++ CourseworkA/your_twitter.R
@@ -3,3 +3,9 @@ consumer_key <-'k9vUb4jWT1EoYRoCEsE3s8vnc'
 consumer_scret <- 'Q6U0Ek1umFvWWGuOVsBOekG7xM5R1cXYkIMx4q85XtWbWqHPo4'
 access_token <- '746835948688805889-q9LEUy2zqL8rigTon9FpqNpH6TkSNvk'
 access_scret <- 'yc3IFL7cQsEMNCLhXuyUvaOv5Wiick0S7scXRU5G0ZICg'
+
+
+#consumer_key <-'S3HE6pNUsmDP3LxMSDgRw8kx8'
+#consumer_scret <- '1aLg11NynVF4q93uaxHuCkd9GpIZxtaePxlvVZoKHrmPjrQIoC '
+#access_token <- '1079739876080340992-O9MHCV8yIbP4fpCXnktPDKTerIk4i8'
+#access_scret <- 'ZYi6mkuPA2zQDw3jqtK9xKvRWc6Yj7xHmCCwMNLpyI0kY'
