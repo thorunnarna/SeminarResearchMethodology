8afbe31ded588323424f67478d2b817d19f39a00
thorunnarna <thorunnarna93@gmail.com>
2019-03-19 10:18:14 +0100

    Coursework A

---
+++ CourseworkA/your_twitter.R
@@ -0,0 +1,5 @@
+#adapt this file based on your twitter app
+consumer_key <-'k9vUb4jWT1EoYRoCEsE3s8vnc'
+consumer_scret <- 'Q6U0Ek1umFvWWGuOVsBOekG7xM5R1cXYkIMx4q85XtWbWqHPo4'
+access_token <- '746835948688805889-q9LEUy2zqL8rigTon9FpqNpH6TkSNvk'
+access_scret <- 'yc3IFL7cQsEMNCLhXuyUvaOv5Wiick0S7scXRU5G0ZICg'
