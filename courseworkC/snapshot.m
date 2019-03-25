13775cf2317f3fa4c4972df8368ca3a2a785de76
Nathan Buskulic <nathan.buskulic@outlook.fr>
2019-03-25 22:54:12 +0100

    snapshot methods added

--- courseworkC/snapshot.m
+++ courseworkC/snapshot.m
@@ -1,17 +1,16 @@
-% We implement the PCA using the second algorithms
+% This is the implementation of the snapshot method
 
-% 1. Get the center of the points
-% 2. Compute the centered points
-% 3. Compute the Gram Matrix
-% 4. Compute eigenvalues and eigenvectors of the Gram Matrix
-% 5. Compute the basis vector of the affine space
+% 1. Select datapoints that will serve as snapshots
+% 2. Build the gram Matrix of these snapshots (using the centered points)
+% 3. Get the eigenvalues and eigenvectors
+% 4. Compute the basis vectors
 
 % For testing purposes
-% 6. We reconstruct the original matrix by projecting on the new affine
-% space.
+% 5. reconstruct the images by projecting on these new basis vectors.
 
+% We get the matrix.
 % -----
-maxImg = 1000
+maxImg = 200
 initMatrix = []
 imagefiles = dir('img_align_celeba/*.jpg');      
 nfiles = length(imagefiles);    % Number of files found
@@ -24,43 +23,49 @@ for ii=1:maxImg
     initMatrix = [initMatrix; x'];
 end
 initMatrix = initMatrix';
-image =uint8(reshape(x + center,h,w,d)*255);
-figure, imshow(image)
-
 % -------
 %%
+
+% Get the snapshots
+% We select them randomly for now
+nbSnapshots = 200;
+indicesSnapshots = randperm(maxImg,nbSnapshots);
+snapshotMatrix = initMatrix(:,indicesSnapshots);
+
 % Get the center
 center = mean(initMatrix,2);
+centeredPoints = initMatrix - center;
 
 % Get the centerd points
-y = initMatrix - center;
+y = snapshotMatrix - center;
 
 % get the Gram Matrix
 gram = y' * y;
 
 % get the eigenvalues and eigenvectors
 [V,D] = eig(gram);
-% We change the order of the vectors
+% We change order the vectors in increasing order
 D = rot90(fliplr(D),-1);
 V = flip(V,2);
 
 % We get the basis vectors
-U = zeros(size(y,1), size(D,1));
+U = zeros(size(initMatrix,1), size(D,1));
 for i = 1:size(D,1)
    U(:,i) = (1/(sqrt(D(i,i)))) * (y * V(:,i));
 end
 U;
 
+%%
 % Testing
 % We project on the new space
-matrixCenter = zeros(size(center,1), size(U,2));
+matrixCenter = zeros(size(initMatrix,1), size(initMatrix,2));
 for i = 1:size(U,2)
    matrixCenter(:,i) = center; 
 end
 
-reconstructedMatrix = (y' * U * U')' + matrixCenter;
+%size(centeredPoints' * U * U')
+reconstructedMatrix = (centeredPoints' * U * U')' + matrixCenter;
 
 % We show the mean image
-image = uint8(reshape(reconstructedMatrix(:,5),h,w,d)*255);
+image = uint8(reshape(reconstructedMatrix(:,973),h,w,d)*255);
 figure, imshow(image)
-
