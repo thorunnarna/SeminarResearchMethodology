5dbaa3ae5fda7311a7de1a0ad78ac68b9beb96af
thorunnarna <thorunnarna93@gmail.com>
2019-03-31 18:18:47 +0200

    images, , flower dataset processing and changes to alorithms

--- courseworkC/PCA_2.m
+++ courseworkC/PCA_2.m
@@ -11,24 +11,65 @@
 % space.
 
 % -----
-maxImg = 1000
+maxImg= 1000
 initMatrix = []
-imagefiles = dir('img_align_celeba/*.jpg');      
-nfiles = length(imagefiles);    % Number of files found
-for ii=1:maxImg
-    currentfilename = imagefiles(ii).name
-    currentimage = imread(strcat('img_align_celeba/',currentfilename));
-    currentimage = rgb2gray(currentimage);
-    [h w d]=size(currentimage);
-    x = double(reshape(currentimage,w*h,d))/255;
-    initMatrix = [initMatrix; x'];
+
+Celeb=1
+Flower = 0
+if Celeb==1
+    imageDir =  'C:/Users/thoru/Downloads/img_align_celeba/img_align_celeba/'
+    ImageDirJpg = imageDir+"*.jpg"
+    imagefiles = dir(ImageDirJpg);      
+    nfiles = length(imagefiles);    % Number of files found
+    for i=1:maxImg
+        currentfilename = imagefiles(i).name
+        currentimage = imread(strcat(imageDir,currentfilename));
+        currentimage = rgb2gray(currentimage);
+        %imshow(currentimage)
+        [h w d]=size(currentimage);
+        x = double(reshape(currentimage,w*h,d))/255;
+        initMatrix = [initMatrix; x'];
+
+    end
+    initMatrix = initMatrix';
+
 end
-initMatrix = initMatrix';
-image =uint8(reshape(x + center,h,w,d)*255);
-figure, imshow(image)
+if Flower==1
+  imageDir =  'C:/Users/thoru/Downloads/102flowers/jpg/'
+  ImageDirJpg = imageDir+"*.jpg"
+  imagefiles = dir(ImageDirJpg);      
+  nfiles = length(imagefiles);    % Number of files found
+  rows =[]
+  columns = []
+  %find the size of all images
+  for i=1:maxImg
+      currentfilename = imagefiles(i).name
+      currentimage = imread(strcat(imageDir,currentfilename));
+      rows = [rows size(currentimage,1)];
+      columns = [columns size(currentimage,2)]  ;  
+  end
+  minRows = min(rows)
+  minColumns = min(columns)
+  %go though all images and trim them so they are all of the same size.
+  for i=1:maxImg
+        currentfilename = imagefiles(i).name
+        currentimage = imread(strcat(imageDir,currentfilename));
+        currentimage = rgb2gray(currentimage);
+        %trim and resize to 150x150 pixles
+        trimmedImg = trimImage(currentimage,minRows,minColumns);
+        trimmedImg =imresize(trimmedImg, [150 150]);
+        [h w d]=size(trimmedImg);
+         %vectorize
+        x = double(reshape(trimmedImg,w*h,d))/255;
+        initMatrix = [initMatrix; x'];
+
+  end
+  initMatrix = initMatrix';
+end
 
 % -------
 %%
+tic
 % Get the center
 center = mean(initMatrix,2);
 
@@ -59,8 +100,23 @@ for i = 1:size(U,2)
 end
 
 reconstructedMatrix = (y' * U * U')' + matrixCenter;
-
+timeGram = toc
 % We show the mean image
-image = uint8(reshape(reconstructedMatrix(:,5),h,w,d)*255);
+image = uint8(reshape(mean(reconstructedMatrix,2),h,w,d)*255);
 figure, imshow(image)
+memory
+
+%calculate difference 
+DiffCov = initMatrix-reconstructedMatrix;
+NormGram =norm(DiffCov)
+function trimmedImg = trimImage(img,rows,columns)
+    CenterRow = size(img,1)/2;
+    CenterColumn = round(size(img,2)/2);
+   
+    row1 = (CenterRow-rows/2)+1;
+    row2 = CenterRow+rows/2;
+    col1 = (CenterColumn-columns/2)+1;
+    col2 = CenterColumn+columns/2;
+    trimmedImg = img(row1:row2, col1:col2);
+end
 
